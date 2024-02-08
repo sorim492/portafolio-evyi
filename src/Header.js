f076c15eaebdcda48e10ab666cf32b6e5f2a9046
sorim492 <sorim492@gmail.com>
2024-02-08 13:08:05 -0600

    add header, main and footer

---
+++ src/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+import logo from './data-logo.png';
+
+export default function Header(){
+return(
+    <div>
+        <img src={logo} className="App-logo" alt="logo" />
+    </div>
+)
+}
---
+++ src/Main.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Main(){
+    return(
+        <div>
+            <h1>Evyenia Mejia</h1>
+            <h2>Data Analyst</h2>
+            <h2>Mechatronics Engineer</h2>
+        </div>
+    )
+}
