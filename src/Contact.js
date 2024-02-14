74c3509422e4e5c95e8e088e96363f002f8cb06e
sorim492 <sorim492@gmail.com>
2024-02-14 15:05:50 -0600

    Update Contact.js

--- src/Contact.js
+++ src/Contact.js
@@ -2,12 +2,19 @@ import React from "react";
 
 export default function Contact(){
 
+    function contactUs(event){
+        event.preventDefault()
+        let name = prompt("What is you name?");
+        let email = prompt("What is your email?");
+
+        alert("Thank you " + name + ", we'll be in touch at " + email +"!")
+    }
+
     return(
         <div className="contact" id="contact">
             <br/>
             <form>
-                <input type="text" className="contact-text"/>
-                <input type="submit" value="contact" className="contact-btn"/>
+                <input type="submit" value="contact" className="contact-btn" onClick={contactUs}/>
             </form>
             <a href="linkedin.com/in/evyeniamm/" target="_blank" rel="noreferrer" className='App-link'><i class="fa-brands fa-linkedin"></i></a>
         </div>
