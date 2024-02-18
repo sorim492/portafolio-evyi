847fdd3956cbd0bba78952c5bd6d088d45c67aae
sorim492 <sorim492@gmail.com>
2024-02-17 20:31:39 -0600

    update certificates

--- src/Certificates.js
+++ src/Certificates.js
@@ -4,10 +4,16 @@ export default function Certificates(){
     return(
         <div className="certificates" id="cer">
             <h2>Certificates</h2>
-            <h3>Data Analyst with Python</h3>
-            <h3>Power BI Fundamentals</h3>
-            <h3>Data Analyst with Power BI</h3>
-            <h3>Kickoff Agile</h3>
+            <div className="cer-grid">
+                <h3><a href="https://www.datacamp.com/statement-of-accomplishment/track/48c1fb0a522044a4c0e25e0f45315a9b8bfed4d0" target="_blank" rel="noreferrer">
+                    Data Analyst with Python
+                </a></h3>
+                <h3><a href="https://www.datacamp.com/statement-of-accomplishment/track/7e45b858059b233ba64552c5c8e18568a129fb12" target="_blank" rel="noreferrer">
+                    Power BI Fundamentals
+                </a></h3>
+                <h3>Data Analyst with Power BI</h3>
+                <h3>Kickoff Agile</h3>
+            </div>
         </div>
     )
 }
