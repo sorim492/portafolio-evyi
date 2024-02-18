b350ed8175148202ddfb147dfebf3fcce126e7d2
sorim492 <sorim492@gmail.com>
2024-02-18 12:29:44 -0600

    Update Contact.js

--- src/Contact.js
+++ src/Contact.js
@@ -1,13 +1,30 @@
-import React from "react";
+import React, {useState} from "react";
 
 export default function Contact(){
+  
+   let [userName, setuserName] = useState("");
+   let [userEmail, setuserEmail] = useState("");
+   let [message, setuserMessage] = useState(""); 
+
+   function updateUsername(event){
+      setuserName(event.target.value);
+   }
+
+   function updateUseremail(event){
+      setuserEmail(event.target.value);
+   }
+
+   function updateUsermessage(event){
+      setuserMessage(event.target.value);
+   }
 
     function contactUs(event){
         event.preventDefault()
-        let name = prompt("What is you name?");
-        let email = prompt("What is your email?");
+        console.log(userName)
+        console.log(userEmail) 
+        console.log(message)
 
-        alert("Thank you " + name + ", we'll be in touch at " + email +"!")
+        alert("Thank you " + userName + ", we'll be in touch at " + userEmail +"!")
     }
 
     return(
@@ -20,25 +37,25 @@ export default function Contact(){
                Got a question or proposal, or just want
                to say hello? Go ahead.
             </h3>
-            <form>
+            <form onSubmit={contactUs}>
                <div className="row">
                   <div className="col-6">
                      <h4>Your name</h4>
-                     <input type="text" placeholder="Enter your name" className="contact-name" id="name" required/>
+                     <input type="text" placeholder="Enter your name" className="contact-name" id="name" required onChange={updateUsername}/>
                   </div>
                   <div className="col-6">
                      <h4>Your email</h4>
-                     <input type="email" placeholder="Enter your email" className="contact-email" id="email" required/>
+                     <input type="email" placeholder="Enter your email" className="contact-email" id="email" required onChange={updateUseremail}/>
                   </div>
                </div>
                <div className="row">
                   <div className="col-12">
                   <h4>Your Message</h4>
-                     <input type="text" placeholder="Hi, we need you, we're Company X" className="contact-message" id="text" required/>
+                     <input type="text" placeholder="Hi, we need you, we're Company X" className="contact-message" id="text" required onChange={updateUsermessage}/>
                   </div>
                </div>
                <br/>
-               <input type="submit" value="Send" className="contact-btn" onClick={contactUs}/>
+               <input type="submit" value="Send" className="contact-btn"/>
             </form>
             <div className="row">
                <div className='col-6'>
