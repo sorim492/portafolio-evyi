b42d8830001e41daf11259d09c6ede8df2daf12b
sorim492 <sorim492@gmail.com>
2024-02-22 16:50:17 -0600

    add new image

--- src/Projects.js
+++ src/Projects.js
@@ -1,11 +1,15 @@
 import React from "react";
 import covid from "./covid tracker.jpeg";
+import habitos from "./habito-lectura.jpeg";
 
 export default function Projects(){
     return(
         <div className="projects" id="projects">
             <h2>Portfolio</h2>
-            <img src={covid} alt="Global covid-19 tracker" className="img-fluid"/>
+            <div className="project-img">
+                <img src={covid} alt="Global covid-19 tracker" className="img-fluid"/>
+                <img src={habitos} alt="habitos de lectura en mexico 2019" className="img-fluid"/>
+            </div>
         </div>
     )
 }
